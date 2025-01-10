ea5063b0fc1a641e56c049bbae3a727f64c72c49
rariizumi <38709409+rariizumi@users.noreply.github.com>
2025-01-10 16:13:48 +0900

    Delete scripts directory

--- scripts/train_icm_p_icm.ps1
+++
@@ -1,4 +0,0 @@
-# icm_second_1
-python ./train.py -c ewalker_free_icm_second_half1.json -o log/free_icm_second_1
-# icm_second_2
-python ./train.py -c ewalker_free_icm_second_half2.json -o log/free_icm_second_2
